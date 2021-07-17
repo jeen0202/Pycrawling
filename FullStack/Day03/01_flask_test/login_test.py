1b16e83ef4e4e7022c23517cf194e7b1ec5bf1f5
sejing <jeen0202@korea.ac.kr>
2021-07-17 18:12:18 +0900

    Day03 connet Flask&bootstrap

--- FullStack/Day03/01_flask_test/login_test.py
+++ FullStack/Day03/01_flask_test/login_test.py
@@ -1,15 +1,14 @@
 from flask import Flask, jsonify, request, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/login')
-def login():
-    username = request.args.get('user_name')
-    passwd = request.args.get('pw')
-    email = request.args.get('email_address')
-    print (username, passwd, email)
+def login():   
+    passwd = request.args.get('passpw')
+    email_address = request.args.get('email_address')
+    print (email_address, passwd)
     
-    if username == 'dave':
+    if email_address == 'sejing@gmail.com':
         return_data = {'auth': 'success'}
     else:
         return_data = {'auth': 'failed'}
@@ -19,8 +18,8 @@ def login():
 @app.route('/html_test')
 def hello_html():
     # html file은 templates 폴더에 위치해야 함
-    return render_template('login.html')
+    return render_template('login_test.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8085")
+    app.run(host="localhost", port="8085")
