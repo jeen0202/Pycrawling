f1cfabe9eedf379728e9042aa0c30889de5eae32
sejing <jeen0202@korea.ac.kr>
2021-07-25 11:56:55 +0900

    Day11 Control(user_info) 구축

---
+++ FullStack/Flask_Blog/blog_control/user_mgmt.py
@@ -0,0 +1,126 @@
+from flask_login import UserMixin
+from db_model.firebase import conn_Firestore
+
+class User(UserMixin):
+
+    def __init__(self,user_id,user_email,blog_id):
+        self.id = user_id
+        self.user_email = user_email
+        self.blog_id = blog_id
+    
+    def get_id(self):
+        return str(self.id)
+
+    @staticmethod
+    def get(user_id):
+        db = conn_Firestore()
+        query = db.collection(u'user_info').where(u'user_id' ,u'==', user_id)
+        #print(query)
+        if not query:
+            return None
+        user= User(user_id = query.user_id, user_email = query.user_email, blog_id= query.blod_id)
+        return user
+    
+    @staticmethod
+    def find(user_email):
+        db = conn_Firestore()
+        query = db.collection(u'user_info').where(u'user_email' ,u'==', user_email)
+        #print(query)
+        if not query:
+            return None
+        user= User(user_id = query.user_id, user_email = query.user_email, blog_id= query.blod_id)
+        return user
+    
+    @staticmethod
+    def create(user_email, blog_id):
+        user = User.find(user_email)
+        if user == None:
+            db = conn_Firestore()
+            db.collection(u'user_info').add(str(user_email), str(blog_id))
+            return User.find(user_email)
+        else :
+            return user
+
+
+
+
+
+
+
+
+
+
+
+
+# class User(UserMixin):
+
+#   def __init__(self,user_id,user_email,blog_id):
+#     self.id = user_id
+#     self.user_email = user_email
+#     self.blog_id = blog_id
+
+#   def get_id(self):
+#     return str(self.id)
+
+#   @staticmethod
+#   def get(user_id):
+#     docs = users_ref.stream()
+#     for doc in docs:
+#       if doc.id == user_id :
+#             user = doc
+#             break
+#     if not user:
+#       return None
+    
+#     print(user)
+#     user = User(user_id=user[0],user_email=user[1], blog_id=user[2])
+
+#   @staticmethod
+#   def create(user_email, blog_id):
+#     user = User.find(user_email)
+#     if user == None :
+#       users_ref.add(
+#         user_email = user_email,
+#         blog_id = blog_id
+#       )
+#       return User.find(user_email)
+#     else :
+#       return user
+
+#   @staticmethod
+#   def find(user_email):
+#     user = users_ref.where('user_email','==',user_email)
+#     if not user:
+#       return None
+    
+#     print(user)
+#     user = User(user_id=user[0], user_email=user[1],blog_id=user[2])
+#     return user
+
+# class BlogSession():
+#   blog_page = {'A': 'blog_A.html', 'B':'blog_B.html'}
+#   session_count = 0
+
+#   @staticmethod
+#   def save_session_info(session_ip,user_email,webpage_name):
+#     now = datetime.now()
+#     now_time = nowstrftime('%d/%m/%Y %H:%M:%S')
+# blogs_ref.add(
+#   session_ip = session_ip,
+#   user_email = user_email,
+#   page = webpage_name,
+#   access_time = now_time
+# )
+
+# @staticmethod
+# def get_blog_page(force=None):
+#       print(force)
+#       if(force == None):
+#             if BlogSession.session_count == 0:
+#               BlogSession.session_count =1
+#               return 'blog_A.html'
+#             else :
+#               BlogSession.session_count = 0
+#               return 'blog_B.html'
+#       else :
+#         return BlogSession.blog_page[force]
