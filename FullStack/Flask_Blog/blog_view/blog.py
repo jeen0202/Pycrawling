f784ffd0e861791394d534ed1fb974b282bd5196
SJ_Win <jeen0202@korea.ac.kr>
2021-07-26 11:31:51 +0900

    Day12 REST 방식을 사용한 data 송수신

--- FullStack/Flask_Blog/blog_view/blog.py
+++ FullStack/Flask_Blog/blog_view/blog.py
@@ -1,6 +1,18 @@
-from flask import Flask, Blueprint, request, render_template
+from flask import Flask, Blueprint, request, render_template,jsonify,make_response,redirect,url_for
+from flask.helpers import make_response
 
 blog_abtest = Blueprint('blog', __name__)
+
+@blog_abtest.route('/set_email', methods=['GET','POST'])
+def set_email():
+    if request.method == "GET":
+        print(request.headers)
+        print('set_email', request.args.get('user_email'))
+    else:
+        print(request.headers)
+        print('set_email', request.form['user_email'])
+        # content tyhpe 이 application/json 일경우 get_json()
+    return redirect('/blog/test')
 
 @blog_abtest.route('/test')
 def test():
