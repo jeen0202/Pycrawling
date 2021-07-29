c352a2222d38e2f2001c0e343e4db92d5b7fdfef
SJ_Win <jeen0202@korea.ac.kr>
2021-07-29 10:16:39 +0900

    Day15 Blog 접속 기록 로깅 구현

--- FullStack/Flask_Blog/blog_view/blog.py
+++ FullStack/Flask_Blog/blog_view/blog.py
@@ -1,6 +1,7 @@
-from flask import Flask, Blueprint, request, render_template,jsonify,make_response,redirect,url_for
+from flask import Flask, Blueprint, request, render_template,jsonify,make_response,redirect,url_for, session
 from flask_login import login_user, current_user,logout_user
 from blog_control.user_mgmt import User
+from blog_control.session_mgmt import BlogSession
 import datetime
 
 blog_abtest = Blueprint('blog', __name__)
@@ -16,18 +17,20 @@ def set_email():
         user = User.create(request.form['user_email'],'A')                
         login_user(user,remember=True,duration=datetime.timedelta(seconds=60))          
         # content tyhpe 이 application/json 일경우 get_json()
-        return redirect(url_for('blog.test'))
+        return redirect(url_for('blog.fullstack1'))
 @blog_abtest.route('logout')
 def logout():
     User.delete(current_user.id)
     logout_user()
-    return redirect(url_for('blog.test'))
+    return redirect(url_for('blog.fullstack1'))
 
 
-@blog_abtest.route('/test')
-def test():    
+@blog_abtest.route('/fullstack1')
+def fullstack1():
+    print(BlogSession.session_count)   
     if current_user.is_authenticated:       
         return render_template('blog_A.html',user_email=current_user.user_email)
     else:
-        print('not authenticated')
-        return render_template('blog_A.html')
+        webpage_name = BlogSession.get_blog_page()
+        BlogSession.save_session_info(session['client_id'], 'anonymous', webpage_name)
+        return render_template(webpage_name)
